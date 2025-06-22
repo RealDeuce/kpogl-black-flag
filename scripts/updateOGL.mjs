044168bb3701a4eefa3f4df0986dda8a572aded5
Stephen Hurd <deuce@synchro.net>
2025-06-22 18:46:22 -0400

    Markdown the line breaks properly.

--- scripts/updateOGL.mjs
+++ scripts/updateOGL.mjs
@@ -4,7 +4,7 @@ fs.readFile('KPjournal/Kobold_Press_OGL.json', function(err, data) {
 	if (err) throw err;
 
 	const entry = JSON.parse(data);
-	const OGL = entry.pages[0].text.content.replace(/<h1>([^<]+)<\/h1>/g, "# $1").replace(/<strong>([^<]+)<\/strong>/g, "**$1**").replace(/<p>/g, "\n").replace(/<br>/g, "\n");
+	const OGL = entry.pages[0].text.content.replace(/<h1>([^<]+)<\/h1>/g, "# $1").replace(/<strong>([^<]+)<\/strong>/g, "**$1**").replace(/<p>/g, "  \n").replace(/<br>/g, "  \n");
 	fs.writeFile('LICENSE.md', OGL, err => {
 		if (err) throw err;
 	});
